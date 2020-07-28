c21bdc552159fe15b5edbc15c0f838f28b47b2a2
toxidxd <toxid13@yanex.ru>
2020-07-28 15:16:02 +0300

    parsing page

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import requests, bs4, random, time, csv
+
 print ('hell')
+#corp link https://zkillboard.com/corporation/98636833/
+link = "https://zkillboard.com/character/2117056606/"
+soup = requests.get(link)
+soup = bs4.BeautifulSoup(soup.text, "html.parser")
+print (soup)
