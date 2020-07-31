952a8c8e85422af31db51afbd60fc065961c2d3b
toxidxd <toxid13@yanex.ru>
2020-07-31 23:11:14 +0300

    added kill parser

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
-import requests, bs4, random, time, csv
+import requests, bs4, random, time, csv, re
 
-print ('hell')
+print ('eve_killboard_parser_by_toxidxd')
 #corp link https://zkillboard.com/corporation/98636833/
-link = "https://zkillboard.com/character/2117056606/"
-soup = requests.get(link)
-soup = bs4.BeautifulSoup(soup.text, "html.parser")
-print (soup)
+#char link https://zkillboard.com/character/2117056606/
+last_id = 0
+link = "https://zkillboard.com"
+soup = requests.get(link) #запрашиваем страницу
+soup = bs4.BeautifulSoup(soup.text, "html.parser") #скармливаем её парсеру
+killid = soup.find_all('tr', {"class": "killListRow"}) #находим все теги tr с заданным классом
+last_id = killid[0]['killid']
+print ("last id at start: ", last_id)
+print ('---------------------------------------')
+
+while True:
+    soup = requests.get(link) #запрашиваем страницу
+    soup = bs4.BeautifulSoup(soup.text, "html.parser") #скармливаем её парсеру
+    print ('---------------------------------------')
+    killid = soup.find_all('tr', {"class": "killListRow"}) #находим все теги tr с заданным классом
+    if killid[0]['killid'] == last_id:
+        print ("no new kills", killid[0]['killid'])
+    else:
+        print (killid[0]['killid'])
+        last_id = killid[0]['killid']
+        print ("new last id", last_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    sl = random.randint(2,5)
+    print("Sleep ", sl, " sec")
+    time.sleep(sl)
