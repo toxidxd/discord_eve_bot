c11295de0464fbe1e08162ae7f48e6b2ec0d25c4
toxidxd <toxid13@yanex.ru>
2020-10-19 21:19:56 +0300

    парсинг json через API

---
+++ deveb_redisq.py
@@ -0,0 +1,24 @@
+import requests, time, re, json
+
+lnk = 'https://redisq.zkillboard.com/listen.php?queueID=toxidxd'
+
+while True:
+    killmail = json.loads(requests.get(lnk).text)
+    #corpid = re.findall(r'victim":{"character_id":\d+,"corporation_id":(\d+)', site.text)
+    #killid = re.findall(r'killID":(\d+)', killmail.text)
+    if killmail["package"] == None:
+        print("is null!")
+    else:
+        killid = killmail["package"]["killID"]
+        vic_cor_id = killmail["package"]["killmail"]["victim"]["corporation_id"]
+        killlnk = "https://zkillboard.com/kill/" + str(killid)
+        if vic_cor_id == 98652899:
+            print(killlnk)
+            print(vic_cor_id)
+        else:
+            print("no victims")
+    time.sleep(1)
+
+
+
+#j["package"]["killmail"]["victim"]["corporation_id"]
