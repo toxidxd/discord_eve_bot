cc5897f05b45dd1e1ddb5620b5f2f9529c89389c
toxidxd <toxid13@yanex.ru>
2020-10-22 17:39:38 +0300

    Added new check of exceptions

--- deveb_redisq.py
+++ deveb_redisq.py
@@ -8,24 +8,27 @@ while True:
     #corpid = re.findall(r'victim":{"character_id":\d+,"corporation_id":(\d+)', site.text)
     #killid = re.findall(r'killID":(\d+)', killmail.text)
     if killmail["package"] is None:
-        print("is null!")
+        print("No new killmails.")
     else:
-        #TODO Проверка атакующего а причастность к корпорации
-
         killid = killmail["package"]["killID"]
         killlnk = "https://zkillboard.com/kill/" + str(killid)
         print(killlnk)
 
         try:
-            atckr_cor_id = killmail["package"]["killmail"]["attackers"][0]["corporation_id"]
-            vic_cor_id = killmail["package"]["killmail"]["victim"]["corporation_id"]
-            print(atckr_cor_id)
-            print(vic_cor_id)
+            atckr_corp_id = killmail["package"]["killmail"]["attackers"][0]["corporation_id"]
+            print(atckr_corp_id)
         except Exception:
-            print("Exception!")
-            print(killmail)
+            atckr_corp_id = 0
+            print("Exception! Attacker is not a member of a corporation.")
 
-        if vic_cor_id == 98652899:
+        try:
+            vic_corp_id = killmail["package"]["killmail"]["victim"]["corporation_id"]
+            print(vic_corp_id)
+        except Exception:
+            vic_corp_id = 0
+            print('Exception! Victim is mot a member of a corporation.')
+
+        if vic_corp_id == 98652899 or atckr_corp_id == 98652899:
             print("It's our man!")
         else:
             print("It's not our man!")
