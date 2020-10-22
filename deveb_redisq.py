33209e33f8b0973c8d80a5b0467588ff9ac6bc60
toxidxd <toxid13@yanex.ru>
2020-10-22 17:24:31 +0300

    Added check for exceptions

--- deveb_redisq.py
+++ deveb_redisq.py
@@ -3,20 +3,33 @@ import requests, time, json
 lnk = 'https://redisq.zkillboard.com/listen.php?queueID=toxidxd'
 
 while True:
+    print('----------------')
     killmail = json.loads(requests.get(lnk).text)
     #corpid = re.findall(r'victim":{"character_id":\d+,"corporation_id":(\d+)', site.text)
     #killid = re.findall(r'killID":(\d+)', killmail.text)
     if killmail["package"] is None:
         print("is null!")
     else:
+        #TODO Проверка атакующего а причастность к корпорации
+
         killid = killmail["package"]["killID"]
-        vic_cor_id = killmail["package"]["killmail"]["victim"]["corporation_id"]
         killlnk = "https://zkillboard.com/kill/" + str(killid)
-        if vic_cor_id == 98652899:
-            print(killlnk)
+        print(killlnk)
+
+        try:
+            atckr_cor_id = killmail["package"]["killmail"]["attackers"][0]["corporation_id"]
+            vic_cor_id = killmail["package"]["killmail"]["victim"]["corporation_id"]
+            print(atckr_cor_id)
             print(vic_cor_id)
+        except Exception:
+            print("Exception!")
+            print(killmail)
+
+        if vic_cor_id == 98652899:
+            print("It's our man!")
         else:
-            print("no victims")
+            print("It's not our man!")
+
     time.sleep(1)
 
 
