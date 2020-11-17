1b44d1ae9b6b5f56c33ddde507c19dbe78a96e6e
toxidxd <toxid13@yandex.ru>
2020-11-17 16:09:11 +0300

    PEP cosmetics

--- disbot.py
+++ disbot.py
@@ -3,42 +3,49 @@ import requests
 import json
 import datetime
 import time
+
 from discord.ext import commands
-####
+
 print("Script started")
-token = "Nzc4MjA3NjIxMzI4Nzk3NzA2.X7OoUw.wY25SxaeQFhESEODCVwyecf-dzk"
-bot = commands.Bot(command_prefix='!') #инициализация бота
+token = "Nzc4MjA3NjIxMzI4Nzk3NzA2.X7OoUw.UqbdHaNDhvKV_sOH1hppDL99pL4"
+bot = commands.Bot(command_prefix='!')  # инициализация бота
+
 
 @bot.event
 async def on_ready():
-	print("Bot is ready")
+	print('Bot Logged in as')
+	print(f"Username:  {bot.user.name}")
+	print(f"User ID:  {bot.user.id}")
+	print('---------------------------------')
+
 
 @bot.command(pass_context=True)
 async def test(ctx, arg):
 	await ctx.send(arg)
+
 
 @bot.command(pass_context=True)
 async def hello(ctx):
 	author = ctx.message.author
 	await ctx.send(f'Hello, {author.mention}')
 
-@bot.command()
-async def fox(ctx,arg):
-	iarg=int(arg)
-	while iarg>0:
-		response = requests.get('https://some-random-api.ml/img/fox') # Get-запр    ос
-		json_data = json.loads(response.text) # Извлекаем JSON
-		embed = discord.Embed(color = 0xff9900, title = 'Random Fox') # Создание Embed'a
-		embed.set_image(url = json_data['link']) # Устанавливаем картинку Embed'a
-		await ctx.send(embed = embed) # Отправляем Embed
-		time.sleep(1)
-		iarg=iarg-1
-		print(iarg)
 
 @bot.command()
-async def kms(ctx): # killmails
-	#@bot.event
-	#async def on_connect(ctx):
+async def fox(ctx, arg):
+	iarg = int(arg)
+	while iarg > 0:
+		response = requests.get('https://some-random-api.ml/img/fox')  # Get-запр    ос
+		json_data = json.loads(response.text)  # Извлекаем JSON
+		embed = discord.Embed(color=0xff9900, title='Random Fox')  # Создание Embed'a
+		embed.set_image(url=json_data['link'])  # Устанавливаем картинку Embed'a
+		await ctx.send(embed=embed)  # Отправляем Embed
+		time.sleep(1)
+		iarg = iarg-1
+		print(iarg)
+
+
+@bot.command()
+async def kms(ctx):  # killmails
 	print("Connetted! Start recieving killmail's.")
 	lnk = 'https://redisq.zkillboard.com/listen.php?queueID=toxidxd_test'
 	while True:
@@ -47,15 +54,12 @@ async def kms(ctx): # killmails
 			print("Request success")
 			now = datetime.datetime.now()
 			print("Time {}:{}:{}".format(now.hour, now.minute, now.second))
-			# corpid = re.findall(r'victim":{"character_id":\d+,"corporation_id":(\d+)', site.text)
-			# killid = re.findall(r'killID":(\d+)', killmail.text)
 			if killmail["package"] is None:
 				print("No new killmails.")
 			else:
 				killid = killmail["package"]["killID"]
 				killlnk = "https://zkillboard.com/kill/" + str(killid)
 				print(killlnk)
-
 				try:
 					atckr_corp_id = killmail["package"]["killmail"]["attackers"][0]["corporation_id"]
 					print('Attacker corp_id:', atckr_corp_id)
@@ -69,7 +73,6 @@ async def kms(ctx): # killmails
 				except Exception:
 					vic_corp_id = 0
 					print('Exception! Victim is mot a member of a corporation.')
-
 				if vic_corp_id > 0 or atckr_corp_id > 0:
 					print("It's our man!")
 					await ctx.send(killlnk)
@@ -81,6 +84,5 @@ async def kms(ctx): # killmails
 		except Exception:
 			print("Request fail")
 			time.sleep(5)
-			break
 
 bot.run(token)
